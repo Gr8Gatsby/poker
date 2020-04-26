4d4699aa3674999f7f410ed8a851d2c276d44fc8
Kevin <kevinhill@live.com>
2020-04-26 14:17:51 -0700

    Created basic HTML, Index.js, & Started Card Class

---
+++ scripts/card.js
@@ -0,0 +1,12 @@
+/*
+    Class: Card
+    Description: This class will store the basic data for 
+    a card, including suit, and value. It will ensure that 
+    the values for a card are valid.
+*/
+class Card {
+  constructor (suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+}
