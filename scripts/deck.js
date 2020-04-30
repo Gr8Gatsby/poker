ef939e78f4f6fd5202fe377b34c689648af280e9
Kevin <kevinhill@live.com>
2020-04-29 19:21:53 -0700

    fix: for loop to refernce Deck static values

--- scripts/deck.js
+++ scripts/deck.js
@@ -4,37 +4,28 @@
 */
 
 class Deck {
+  deck = [];
+  static suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+  static values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
 
-    deck = [];
-    static suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
-    static values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  // Construct deck
+  constructor () {
+    this.shuffle ();
+  }
 
-    // Construct deck
-    constructor() {
-        this.shuffle()
-        
+  // Shuffle method
+  shuffle () {
+    for (let suit in Deck.suits) {
+      for (let value in Deck.values) {
+        this.deck.push (`${Deck.values[value]} of ${Deck.suits[suit]}`);
+      }
     }
+  }
 
-    // Shuffle method
-    shuffle() {
-        for (let suit in this.suits) {
-                  for (let value in this.values) {
-                    this.deck.push(`${this.values[value]} of ${this.suits[suit]}`);
-                  }
-                }
-    }
-    
+  // Initailize method
+  initialize () {}
 
-    // Initailize method
-    initialize() {
-
-    }
-
-    // Deal method
-
-
+  // Deal method
 }
 
-class Hand {
-
-}
+class Hand {}
